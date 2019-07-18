8f6a90ed538a6ebcc11d13ecd0143548d8ee07f4
Safarali Ramazonov <mr.safarali@gmail.com>
2019-07-18 12:59:52 -0600

    Remove unused imports

--- src/components/RepositoryList.js
+++ src/components/RepositoryList.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import RepositoryItem from './RepositoryItem';
-import Button from './Button';
-import Loading from './Loading';
 import FetchMore from './FetchMore';
 
 const RepositoryList = ({ repositories, fetchMore, loading }) => {
